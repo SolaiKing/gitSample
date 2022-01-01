aa0df9395b8415b9ea395184754b2c8decdd0e7e
SolaiKing <solaiskat@gmail.com>
2022-01-01 11:40:52 +0530

    task done

---
+++ src/gitSample/Task1.java
@@ -0,0 +1,12 @@
+package gitSample;
+
+public class Task1 {
+	
+	public static void username() {
+     System.out.println("username");
+	}
+	public static void main(String[] args) {
+		username();
+	}
+
+}
